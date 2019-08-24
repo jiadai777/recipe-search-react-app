a556c69b2ecacf1b866f5a2c435dbfc84bdbc9ce
Jiadai Liu <jliuapp@gmail.com>
2019-08-24 09:58:15 -0700

    Add files via upload

---
+++ src/components/Recipe.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Recipe extends React.Component {
+
+	render() {
+		
+
+		return (
+			<React.Fragment>
+			hello from recipe
+			</React.Fragment>
+		);
+	}
+}
---
+++ src/components/RecipeDetails.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {recipe} from '../tempDetails';
+
+export default class RecipeDetails extends React.Component {
+
+	render() {
+		return (
+			<React.Fragment>
+			hello from recipe details
+			</React.Fragment>
+		);
+	}
+}
